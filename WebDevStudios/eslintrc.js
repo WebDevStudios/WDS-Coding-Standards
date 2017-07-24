50557c5d5d0aff0e5122ac0eed17eaa32d387404
Aubrey Portwood <aubreypwd@gmail.com>
2017-07-24 13:25:37 -0700

    Docblock each rule so we can keep track of when each was introduced by who.

--- WebDevStudios/eslintrc.js
+++ WebDevStudios/eslintrc.js
@@ -8,105 +8,243 @@ module.exports = {
 	'plugins': [],
 	'rules': {
 
-		// Enforce spacing inside array brackets
+		/**
+		 * Enforce spacing inside array brackets.
+		 *
+		 * @author Aubrey Portwood
+		 * @since 1.0
+		 */
 		'array-bracket-spacing': [ 'error', 'always' ],
 
-		// Enforce one true brace style
+		/**
+		 * Enforce one true brace style.
+		 *
+		 * @author Aubrey Portwood
+		 * @since 1.0
+		 */
 		'brace-style': 'error',
 
-		// Require camel case names
+		/**
+		 * Require camel case names.
+		 *
+		 * @author Aubrey Portwood
+		 * @since 1.0
+		 */
 		'camelcase': [ 'error', {
 			properties: 'always'
 		} ],
 
-		// Disallow or enforce trailing commas
+		/**
+		 * Disallow or enforce trailing commas.
+		 *
+		 * @author Aubrey Portwood
+		 * @since 1.0
+		 */
 		'comma-dangle': [ 'error', 'never' ],
 
-		// Enforce spacing before and after comma
+		/**
+		 * Enforce spacing before and after comma.
+		 *
+		 * @author Aubrey Portwood
+		 * @since 1.0
+		 */
 		'comma-spacing': 'error',
 
-		// Enforce one true comma style
+		/**
+		 * Enforce one true comma style.
+		 *
+		 * @author Aubrey Portwood
+		 * @since 1.0
+		 */
 		'comma-style': [ 'error', 'last' ],
 
-		// Encourages use of dot notation whenever possible
+		/**
+		 * Encourages use of dot notation whenever possible.
+		 *
+		 * @author Aubrey Portwood
+		 * @since 1.0
+		 */
 		'dot-notation': [ 'error', {
 			allowKeywords: true,
 			allowPattern: '^[a-z]+(_[a-z]+)+$'
 		} ],
 
-		// Enforce newline at the end of file, with no multiple empty lines
+		/**
+		 * Enforce newline at the end of file, with no multiple empty lines.
+		 *
+		 * @author Aubrey Portwood
+		 * @since 1.0
+		 */
 		'eol-last': 'error',
 
-		// Require or disallow spacing between function identifiers and their invocations
+		/**
+		 * Require or disallow spacing between function identifiers and their invocations.
+		 *
+		 * @author Aubrey Portwood
+		 * @since 1.0
+		 */
 		'func-call-spacing': 'off',
 
-		// Enforces spacing between keys and values in object literal properties
+		/**
+		 * Enforces spacing between keys and values in object literal properties.
+		 *
+		 * @author Aubrey Portwood
+		 * @since 1.0
+		 */
 		'key-spacing': [ 'error', {
 			beforeColon: false,
 			afterColon: true
 		} ],
 
-		// Enforce spacing before and after keywords
+		/**
+		 * Enforce spacing before and after keywords.
+		 *
+		 * @author Aubrey Portwood
+		 * @since 1.0
+		 */
 		'keyword-spacing': 'error',
 
-		// Disallow mixed "LF" and "CRLF" as linebreaks
+		/**
+		 * Disallow mixed "LF" and "CRLF" as linebreaks.
+		 *
+		 * @author Aubrey Portwood
+		 * @since 1.0
+		 */
 		'linebreak-style': [ 'error', 'unix' ],
 
-		// Enforces empty lines around comments
+		/**
+		 * Enforces empty lines around comments.
+		 *
+		 * @author Aubrey Portwood
+		 * @since 1.0
+		 */
 		'lines-around-comment': [ 'error', {
 			beforeLineComment: true
 		} ],
 
-		// Disallow mixed spaces and tabs for indentation
+		/**
+		 * Disallow mixed spaces and tabs for indentation.
+		 *
+		 * @author Aubrey Portwood
+		 * @since 1.0
+		 */
 		'no-mixed-spaces-and-tabs': 'error',
 
-		// Disallow use of multiline strings
+		/**
+		 * Disallow use of multiline strings.
+		 *
+		 * @author Aubrey Portwood
+		 * @since 1.0
+		 */
 		'no-multi-str': 'error',
 
-		// Disallow multiple empty lines
+		/**
+		 * Disallow multiple empty lines.
+		 *
+		 * @author Aubrey Portwood
+		 * @since 1.0
+		 */
 		'no-multiple-empty-lines': 'error',
 
-		// Disallow use of the with statement
+		/**
+		 * Disallow use of the with statement.
+		 *
+		 * @author Aubrey Portwood
+		 * @since 1.0
+		 */
 		'no-with': 'error',
 
-		// Require or disallow an newline around variable declarations
+		/**
+		 * Require or disallow an newline around variable declarations.
+		 *
+		 * @author Aubrey Portwood
+		 * @since 1.0
+		 */
 		'one-var-declaration-per-line': [ 'error', 'initializations' ],
 
-		// Enforce operators to be placed before or after line breaks
+		/**
+		 * Enforce operators to be placed before or after line breaks.
+		 *
+		 * @author Aubrey Portwood
+		 * @since 1.0
+		 */
 		'operator-linebreak': [ 'error', 'after' ],
 
-		// Require or disallow use of semicolons instead of ASI
+		/**
+		 * Require or disallow use of semicolons instead of ASI.
+		 *
+		 * @author Aubrey Portwood
+		 * @since 1.0
+		 */
 		'semi': [ 'error', 'always' ],
 
-		// Require or disallow space before blocks
+		/**
+		 * Require or disallow space before blocks.
+		 *
+		 * @author Aubrey Portwood
+		 * @since 1.0
+		 */
 		'space-before-blocks': [ 'error', 'always' ],
 
-		// Require or disallow space before function opening parenthesis
+		/**
+		 * Require or disallow space before function opening parenthesis.
+		 *
+		 * @author Aubrey Portwood
+		 * @since 1.0
+		 */
 		'space-before-function-paren': [ 'error', 'never' ],
 
-		// Require or disallow space before blocks
+		/**
+		 * Require or disallow space before blocks,
+		 *
+		 * @author Aubrey Portwood
+		 * @since  1.1
+		 */
 		'space-in-parens': [ 'error', 'always' ],
 
-		// Require spaces around operators
+		/**
+		 * Require spaces around operators,
+		 *
+		 * @author Aubrey Portwood
+		 * @since 1.1
+		 */
 		'space-infix-ops': 'error',
 
-		// Require or disallow spaces before/after unary operators (words on by default, nonwords)
+		/**
+		 * Require or disallow spaces before/after unary operators (words on by default, nonwords),
+		 *
+		 * @author Aubrey Portwood
+		 * @since  1.1
+		 */
 		'space-unary-ops': [ 'error', {
 			overrides: { '!': true }
 		} ],
 
-		/*
-		 * Warns to declare all vars on top of their containing scope.
+		/**
+		 * Don't force vars to be on top.
 		 *
-		 * We've changed this from error to warn as WDSer's are allowed to var
-		 * as they wish, but since it's enforced by Core, we warn here.
+		 * In contradiction to https://make.wordpress.org/core/handbook/best-practices/coding-standards/javascript/#declaring-variables-with-var
+		 * we do not require this.
+		 *
+		 * @since  1.1
+		 * @author Aubrey Portwood
 		 */
-		'vars-on-top': 'warn',
+		'vars-on-top': 'off',
 
-		// Require or disallow Yoda conditions
+		/**
+		 * Require or disallow Yoda conditions.
+		 *
+		 * @since  1.0
+		 * @author Aubrey Portwood
+		 */
 		'yoda': [ 'error', 'always' ],
 
-		// Always show an error when a variable is created that is never used.
+		/**
+		 * Always show an error when a variable is created that is never used.
+		 *
+		 * @author Aubrey Portwood
+		 * @since  1.1
+		 */
 		'no-unused-vars': 'error'
 	}
 };
