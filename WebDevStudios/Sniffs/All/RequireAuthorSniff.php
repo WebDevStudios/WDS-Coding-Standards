658aa0aa78e79691f9af8a87e5b64aade43bf0e5
Aubrey Portwood <aubreypwd@gmail.com>
2018-10-15 12:55:46 -0700

    Require @author in all docblocks.

--- WebDevStudios/Sniffs/All/RequireAuthorSniff.php
+++ WebDevStudios/Sniffs/All/RequireAuthorSniff.php
@@ -1,10 +1,10 @@
 <?php // @codingStandardsIgnoreLine: Filename is ok.
 /**
- * Require the since tag.
+ * Require the author tag.
  *
  * The since tag is required on all docblock elements.
  *
- * @since   1.1.0
+ * @since   1.2.0
  * @package WebDevStudios\Sniffs
  */
 
@@ -17,15 +17,15 @@ use PHP_CodeSniffer_File;
  * Require the return tag.
  *
  * @author Aubrey Portwood
- * @since  1.1.0
+ * @since  1.2.0
  */
-class RequireSinceSniff extends BaseSniff {
+class RequireAuthorSniff extends BaseSniff {
 
 	/**
 	 * What are we parsing?
 	 *
 	 * @author Aubrey Portwood
-	 * @since  1.1.0
+	 * @since  1.2.0
 	 *
 	 * @var array
 	 */
@@ -38,7 +38,7 @@ class RequireSinceSniff extends BaseSniff {
 	 * Register on all docblock comments.
 	 *
 	 * @author Aubrey Portwood
-	 * @since  1.1.0
+	 * @since  1.2.0
 	 *
 	 * @return array List of tokens.
 	 */
@@ -50,7 +50,7 @@ class RequireSinceSniff extends BaseSniff {
 			 *
 			 * @link http://php.net/manual/en/language.basic-syntax.comments.php
 			 *
-			 * @since 1.1.0
+			 * @since 1.2.0
 			 */
 			T_DOC_COMMENT_OPEN_TAG,
 		];
@@ -59,46 +59,28 @@ class RequireSinceSniff extends BaseSniff {
 	/**
 	 * Process file.
 	 *
-	 * @author Aubrey Portwood
-	 *
 	 * @param  PHP_CodeSniffer_File $file            The file object.
 	 * @param  int                  $doc_block_start Where the docblock starts.
 	 *
-	 * @since 1.1.0
-	 *
-	 * @since 1.1.1 Don't warn about @since about anything in themes.
-	 * @see         https://docs.google.com/document/d/16-wN2i9Fe2fpq24PMMQqu80vBvCVNvm2kpgwtcfsJXE/edit Documentation of this requirement.
-	 *
-	 * @return void Early bail if a WordPress theme.
+	 * @since 1.2.0
 	 */
 	public function process( PHP_CodeSniffer_File $file, $doc_block_start ) {
 		$this->tokens  = $file->getTokens();
 		$token         = $this->tokens[ $doc_block_start ];
 		$doc_block_end = $token['comment_closer'];
 
-		// The @since in the comment block, false by default.
+		// The @author in the comment block, false by default.
 		$have_an_at_since_tag = false;
-
 		for ( $i = $doc_block_start; $i <= $doc_block_end; $i++ ) {
-			if ( stristr( $this->tokens[ $i ]['content'], '@since' ) ) {
+			if ( stristr( $this->tokens[ $i ]['content'], '@author' ) ) {
 
-				// We found an @since in the block.
+				// We found an @author in the block.
 				$have_an_at_since_tag = $this->tokens[ $i ];
 			}
 		}
 
-		/**
-		 * Don't warn on theme files at all.
-		 *
-		 * @since 1.1.1
-		 * @see   https://docs.google.com/document/d/16-wN2i9Fe2fpq24PMMQqu80vBvCVNvm2kpgwtcfsJXE/edit Documentation of this requirement.
-		 */
-		if ( stristr( $file->getFilename(), 'wp-content/themes/' ) ) {
-			return;
-		}
-
 		if ( ! $have_an_at_since_tag ) {
-			$this->warn( $file, $doc_block_end, sprintf( "Documenting the version this was introduced is recommended. If you aren't using any official versioning standard, consider using the date, e.g.: %s.", date( 'F j, Y' ) ) );
+			$this->error( $file, $doc_block_end, 'Documenting @author is helpful. If the author is unknown, you can use @author Unknown.' );
 		}
 	}
 }
